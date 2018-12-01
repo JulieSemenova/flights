3804d60905ae62701886626f379daa8ae3ef5629
JulieSemenova <julie.logg@gmail.com>
2018-12-01 15:10:11 +0300

    💄 copyright

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as format from 'date-fns/format';
 import { connect } from 'react-redux';
 import { Tabs, Select } from 'antd';
+import axios from 'axios';
 
 import ArrivalsTab from '../ArrivalsTab/ArrivalsTab';
 import DelaysTab from '../DelaysTab/DelaysTab';
@@ -19,9 +20,9 @@ import {
 } from '../../types';
 import { getDictionary } from '../../redux/reducers/dictionary';
 
-import './Main.css';
-import { FORMAT_FULL_DAY } from '../../constants';
+import { FORMAT_FULL_DAY, API_KEY } from '../../constants';
 import getISODate from '../../helpers/getISODate';
+import './Main.css';
 
 const TabPane = Tabs.TabPane;
 const Option = Select.Option;
@@ -35,23 +36,59 @@ interface IState {
   airportCode: AirportCode;
   date: ISOString;
   searchFlights: Array<IFlights.Flight>;
+  copyright: any;
 }
+
+
+
 export class Main extends React.Component<IProps, IState> {
   state: IState = {
     language: 'ru',
     airportCode: 's9600216',
     date: format(new Date(), FORMAT_FULL_DAY),
-    searchFlights: []
+    searchFlights: [], 
+    copyright: {}
   };
 
   componentDidMount() {
     this.props.getDictionary('ru');
+    axios
+      .get(
+        `/copyright/?apikey=${API_KEY}&format=json`
+      )
+      .then(response => this.setState({copyright: response.data.copyright}))
+      .catch(error => console.log(error));
   }
 
   componentDidUpdate(prevProps: IProps, prevState: IState) {
     if (prevState.language !== this.state.language) {
       this.props.getDictionary(this.state.language);
     }
+  }
+
+  renderCopyrights = () => {
+    const { copyright } = this.state;
+    if (copyright) {
+      return (
+        <div>
+          <div key={copyright.logo_hy} dangerouslySetInnerHTML={{ __html:copyright.logo_hy }} />
+          <p>{copyright.text} {' '} <a href={copyright.upl}> {copyright.url}</a></p>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  getAttrs = (iframeTag: any) => {
+    const doc = document.createElement('div');
+    doc.innerHTML = iframeTag;
+    const iframe = doc.getElementsByTagName('iframe')[0];
+    return [].slice
+      .call(iframe.attributes)
+      .reduce((attrs: any, element: any) => {
+        attrs[element.name] = element.value;
+        return attrs;
+      }, {});
   }
 
   renderLanguageSelect = () => (
@@ -115,7 +152,7 @@ export class Main extends React.Component<IProps, IState> {
     const Component: React.ComponentClass<ITabs.TabProps> = tab.component;
     return (
       <TabPane tab={<GetTranslation word={tab.name} />} key={tab.name}>
-      <Component {...this.state} />}
+        <Component {...this.state} />
       </TabPane>
     );
   };
@@ -146,13 +183,12 @@ export class Main extends React.Component<IProps, IState> {
 
     return (
       <div className="main">
-
         {this.renderAirportSelect()}
         {this.renderDateSelect()}
         {this.renderLanguageSelect()}
         <Search {...this.state} getFlights={this.getFlights} />
         <Tabs>{tabs.map(this.renderPane)}</Tabs>
-
+        {this.renderCopyrights()}
       </div>
     );
   }
